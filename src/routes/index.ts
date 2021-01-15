15c40b3691d6d7f23efdc4490bb77bf0505662d6
Rodrigo Ribeiro <suporte.rodrigoribeiro@gmail.com>
2021-01-15 17:54:08 -0300

    atualizando desafio 6 para entrega

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,7 @@
 import { Router } from 'express';
-
 import transactionsRouter from './transactions.routes';
 
 const routes = Router();
-
 routes.use('/transactions', transactionsRouter);
 
 export default routes;
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,19 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
+import TransctionRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransctionRepository);
+
+    const transaction = await transactionRepository.findOne(id);
+
+    if (!transaction) {
+      throw new Error('Transaction dows not exists');
+    }
+
+    await transactionRepository.remove(transaction);
   }
 }
 
